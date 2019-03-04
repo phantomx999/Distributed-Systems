ea354fb09a12c6ca2ffd072ebb4734706aba2f68
Jeff D <dicki098@csel-kh1250-32.cselabs.umn.edu>
2019-03-04 11:42:29 -0600

    working thrift file

--- project1/project1.thrift
+++ project1/project1.thrift
@@ -1,48 +1,44 @@
-namespace cpp project1 {
+namespace cpp project1
 
-struct Job_struct (
-  1: i64 var1;
-  2: vector<string> var2;
-  3: i64: var3;
-  4: vector<NodeClient> var4;
-) 
+struct Job_struct {
+  1: i64 var1,
+  2: list<string> var2,
+  3: i64 var3,
+  4: list<Node_struct> var4
+}
 
-struct Node_struct (
-  1: std::string* var1;
-  2: std::string* var2;
-  3: vector<task> var3;  
+struct Node_struct {
+  1: string var1,
+  2: string var2,
+  3: list<Task_struct> var3
 //6: map<std::string* s, float val> var5;
-)
+}
 
-typedef struct Task_struct (
-  1: i64 var1;
-  2: i64 var2;
-  3: std::string* val3;
-  4: float var4;
-  5: std::string var5;
-  6: std::string var6;
-) task;
+struct Task_struct {
+  1: i64 var1,
+  2: i64 var2,
+  3: string val3,
+  4: double var4,
+  5: string var5,
+  6: string var6
+}
 
 service Job {
-  void CountFiles(1:std::string var1);
-  std::string PerformJob(1:std::string input) {
+  void CountFiles(1:string var1),
+  string PerformJob(1:string input)
 }
 
 service Node {
-  void StorePositiveWords();
-  void StoreNegativeWords();
-  //map<std::string* s, float val> CreateIntermediateFile()  {
+  void StorePositiveWords(),
+  void StoreNegativeWords()
+  //map<std::string* s, float val> CreateIntermediateFile()
 }
 
 service Task {
-  void CreateFileName(); 
-  float CalculateSentiment();
-  void Map(1:std::string filename, 2:std::string* positives, 3:std::string* negatives);
-  std::string* Reduce(1:std::string* filenames);
-  std::string MakeIntermediateFile(1:std::string input_file_name);
-  std::string MakeOutputResult (1:std::string* lists);
+  void CreateFileName(),
+  double CalculateSentiment(),
+  void Map(1:string filename, 2:string positives, 3:string negatives),
+  string Reduce(1:string filenames),
+  string MakeIntermediateFile(1:string input_file_name),
+  string MakeOutputResult (1:string lists)
 }
-
-}
-
-
